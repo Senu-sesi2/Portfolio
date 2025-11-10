46f20cc11ebb890c0c35fd39127fa049245b5711
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-10 05:24:17 +0000

    style the navbar and add font family

--- portfoliosite/src/components/Navbar.js
+++ portfoliosite/src/components/Navbar.js
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom"
-import "./NavbarStyles.css"
+import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
 
 import React from 'react'
+
 
 const Navbar = () => {
   return (
@@ -9,6 +10,20 @@ const Navbar = () => {
         <Link to="/">
             <h1>Portfolio</h1>
         </Link>
+        <ul className="nav-menu">
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/about">About</Link>
+            </li>
+            <li>
+                <Link to="/project">Project</Link>
+            </li>
+            <li>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
     </div>
   )
 }
