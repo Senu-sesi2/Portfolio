c82f3e5718b001c8fc0d280c308b4537802cb65a
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-11 13:15:42 +0000

    add image to hero setion

--- portfoliosite/src/components/HeroImg.js
+++ portfoliosite/src/components/HeroImg.js
@@ -1,9 +1,14 @@
 import "./HeroImgStyles.css";
 import React from 'react'
+import PortImg from "../assets/port.png"
 
 const HeroImg = () => {
   return (
     <div className="hero">
+        <div className="mask">
+            <img className="port" src={PortImg} 
+            alt="Software engineer in software engineering setting" />
+        </div>
       
     </div>
   )
