2c2d921a04c5c269c7ea56007265c23ae6bee5d4
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-11 13:36:18 +0000

    implement hero setion

--- portfoliosite/src/components/HeroImg.js
+++ portfoliosite/src/components/HeroImg.js
@@ -1,6 +1,7 @@
 import "./HeroImgStyles.css";
 import React from 'react'
 import PortImg from "../assets/port.png"
+import { Link } from "react-router-dom";
 
 const HeroImg = () => {
   return (
@@ -9,7 +10,16 @@ const HeroImg = () => {
             <img className="port" src={PortImg} 
             alt="Software engineer in software engineering setting" />
         </div>
-      
+      <div>
+        <p>Hi, <img width="48" height="48" 
+        src="https://img.icons8.com/emoji/48/waving-hand-light-skin-tone.png" 
+        alt="waving-hand-light-skin-tone"/>I am Emmanuel Mensah</p>
+        <h1>A Software Engineer.</h1>
+        <div>
+          <Link to="/project" className="btn">PROJECTS</Link>
+          <Link to="/contact" className="btn btn-light">CONTACT</Link>
+        </div>
+      </div>
     </div>
   )
 }
