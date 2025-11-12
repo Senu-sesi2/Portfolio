aea08975cac802a64290127afd628a044c238a1f
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-12 16:04:00 +0000

    add footer info

--- portfoliosite/src/components/Footer.js
+++ portfoliosite/src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaHome } from "react-icons/fa"
+import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa"
 import "./FooterStyles.css"
 
 import React from 'react'
@@ -11,6 +11,20 @@ const Footer = () => {
             <div className="location">
                 <FaHome size={20} 
                 style={{color: "whitesmoke", marginRight: "2rem" }} />
+                <div>
+                    <p>B313/4 Kwasheman Rd.</p>
+                    <p>Accra</p>
+                </div>
+            </div>
+            <div className="phone">
+                <h4><FaPhone size={20} 
+                style={{color: "whitesmoke", marginRight: "2rem" }} />
+                (+233) 59 651 9210 / 55 427 9266</h4>
+            </div>
+            <div className="email">
+                <h4><FaMailBulk size={20} 
+                style={{color: "whitesmoke", marginRight: "2rem" }} />
+                senu.e30@gmail.com</h4>
             </div>
         </div>
 
