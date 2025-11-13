b0610e8883dded635fe60e2e0a1422afe48f69dc
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-13 15:48:47 +0000

    add project cards section

--- portfoliosite/src/routes/Project.js
+++ portfoliosite/src/routes/Project.js
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import HeroImg2 from '../components/HeroImg2'
 import TechStackCards from '../components/TechStackCards'
+import ProjectCard from '../components/ProjectCard'
 
 const Project = () => {
   return (
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Featured Portfolio Pieces"/>
+      <ProjectCard />
       <TechStackCards />
       <Footer />
     </div>
