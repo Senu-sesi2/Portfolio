4aeabdf44c6e3df902f5e05a58bfd33bea4498cd
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-13 07:56:29 +0000

    add footer info

--- portfoliosite/src/components/Footer.js
+++ portfoliosite/src/components/Footer.js
@@ -1,4 +1,4 @@
-import { FaHome, FaMailBulk, FaPhone } from "react-icons/fa"
+import { FaFacebook, FaHome, FaLinkedin, FaMailBulk, FaPhone, FaTwitter } from "react-icons/fa"
 import "./FooterStyles.css"
 
 import React from 'react'
@@ -19,16 +19,30 @@ const Footer = () => {
             <div className="phone">
                 <h4><FaPhone size={20} 
                 style={{color: "whitesmoke", marginRight: "2rem" }} />
-                (+233) 59 651 9210 / 55 427 9266</h4>
+                +233 59 651 9210</h4>
             </div>
             <div className="email">
-                <h4><FaMailBulk size={20} 
+                <h4>
+                  <FaMailBulk size={20} 
                 style={{color: "whitesmoke", marginRight: "2rem" }} />
                 senu.e30@gmail.com</h4>
             </div>
         </div>
 
-        <div className="right"></div>
+        <div className="right">
+          <h4>About Me</h4>
+          <p>Certified Software Engineer passionate about building scalable, 
+            AI-driven solutions. Combines technical expertise with digital marketing 
+            insight to create innovative, data-powered products.</p>
+            <div className="social">
+            <FaFacebook size={30} 
+                style={{color: "whitesmoke", marginRight: "1rem" }} />
+              <FaTwitter size={30} 
+                style={{color: "whitesmoke", marginRight: "1rem" }} />
+              <FaLinkedin size={30} 
+                style={{color: "whitesmoke", marginRight: "1rem" }} />
+            </div>
+        </div>
       </div>
     </div>
   )
