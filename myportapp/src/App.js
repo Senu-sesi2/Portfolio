563ff2f3d4169fcefdab3e48944a8a0992057a09
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-15 22:22:54 +0000

    add components folder

--- myportapp/src/App.js
+++ myportapp/src/App.js
@@ -4,13 +4,19 @@ import Home from "./routes/Home"
 import About from "./routes/About"
 import Project from "./routes/Project"
 import Contact from "./routes/Contact"
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+    </>
   );
 }
 
---
+++ myportapp/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import "./NavbarStyles.css"
+
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="header"></div>
+  )
+}
+
+export default Navbar
