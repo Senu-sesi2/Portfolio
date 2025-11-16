c6f236cf92a86a8295de60c5a713e18b32a7c476
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-16 14:01:15 +0000

    create the footer component and style it

--- myportapp/src/routes/Home.js
+++ myportapp/src/routes/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSec from "../components/HeroSec"
+import Footer from './../components/Footer';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSec />
+      <Footer />
     </div>
   )
 }
