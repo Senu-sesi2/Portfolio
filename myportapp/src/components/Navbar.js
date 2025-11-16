5219ae05434741f006f2d0cabe5178a5260d259a
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-16 03:23:59 +0000

    add and style  navbar component

--- myportapp/src/components/Navbar.js
+++ myportapp/src/components/Navbar.js
@@ -1,10 +1,42 @@
+import { Link } from "react-router-dom"
 import "./NavbarStyles.css"
 
-import React from 'react'
+import React, { useState } from 'react'
+
+import {FaBars, FaTimes} from "react-icons/fa"
+
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+
+
   return (
-    <div className="header"></div>
+    <div className="header">
+      <Link to="/">
+        <h1>Portfolio</h1>
+      </Link>
+      <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/project">Projects</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+      <div className="hamburger" onClick={handleClick}>
+        {click ? (<FaTimes size={20} style={{ color: "whitesmoke" }} />
+      ) : (
+      <FaBars size={20} style={{ color: "whitesmoke" }}/>)}
+      </div>
+    </div>
   )
 }
 
