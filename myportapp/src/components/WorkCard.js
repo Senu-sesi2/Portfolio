ba52dd18d693a955efbf06f8f3ac9edd1a089f97
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-18 15:29:12 +0000

    add image to work card and style

--- myportapp/src/components/WorkCard.js
+++ myportapp/src/components/WorkCard.js
@@ -1,6 +1,8 @@
 import "./WorkCardStyles.css"
 
 import React from 'react'
+import pro1 from "../assets/project1.png"
+import { NavLink } from "react-router-dom"
 
 const WorkCard = () => {
   return (
@@ -8,7 +10,15 @@ const WorkCard = () => {
         <h1 className="project-heading">Projects</h1>
         <div className="project-container">
             <div className="project-card">
-                
+                <img src={pro1} alt="responsive to-do app" />
+                <h2 className="project-title">Project Title</h2>
+                <div className="pro-details">
+                  <p>This is the test</p>
+                  <div className="pro-btns">
+                    <NavLink to="url.com" className="btn">Learn More</NavLink>
+                    <NavLink to="url.com" className="btn">Comment</NavLink>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
