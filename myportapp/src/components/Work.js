f8303e6f62b510d233e37c5c7b1a1b039a776360
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-19 16:02:08 +0000

    add WorkCardData

--- myportapp/src/components/Work.js
+++ myportapp/src/components/Work.js
@@ -4,7 +4,7 @@ import React from 'react'
 import pro1 from "../assets/project1.png"
 import { NavLink } from "react-router-dom"
 
-const WorkCard = () => {
+const Work = () => {
   return (
     <div className="work-container">
         <h1 className="project-heading">Projects</h1>
@@ -25,4 +25,4 @@ const WorkCard = () => {
   )
 }
 
-export default WorkCard
+export default Work
---
+++ myportapp/src/components/WorkCardData.js
@@ -0,0 +1,38 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+import pro4 from "../assets/project4.png"
+import pro5 from "../assets/project5.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Next ToDo Application",
+        text: "NextToDo is a simple, efficient to-do app designed tohelp you stay organized and productive. With intuitive features and a clean interface, it ensures your tasks are easy to manage and prioritize.",
+        view: "https://github.com/Senu-sesi2/Portfolio/tree/main/NextToDo"
+    },
+    {
+        imgsrc: pro2,
+        title: "GateKeeper - Sign-in and Sign-up form",
+        text: "GateKeeper is a sleek and interactive Sign-in and Sign-up form built with web technologies such as HTML, CSS, and JavaScript. It combines functionality with engaging design, providing users with an exceptional authentication experience",
+        view: "https://github.com/Senu-sesi2/Portfolio/tree/main/GateKeeper"
+    },
+    {
+        imgsrc: pro3,
+        title: "PayPort - Responsive Payment Gateway Form",
+        text: "PayPort is a responsive payment gateway application designed to provide a seamless and secure checkout experience. It supports multiple payment methods and includes essential features to ensure efficient handling of payment and billing information.",
+        view: "https://github.com/Senu-sesi2/Portfolio/tree/main/PayPort"
+    },
+    {
+        imgsrc: pro4,
+        title: "TastyConnect - Recipe Finding & Sharing App ",
+        text: "Tasty Connect is a web application where individuals can visit to upload and access recipes for different kinds of cuisines. Built with Python, Edamam recipe finding API, HTML, CSS and JS",
+        view: "https://github.com/Senu-sesi2/Portfolio/tree/main/TastyConnect-main/TastyConnect-main"
+    },
+    {
+        imgsrc: pro5,
+        title: "TasteAfric - Webstack-Portfolio-Project",
+        text: "TasteAfric is a web application dedicated to showcasing the rich diversity of African cuisine. It offers a platform where users can explore, share, and discover traditional and contemporary African recipes from across the continent.",
+        view: "https://github.com/Senu-sesi2/Portfolio/tree/main/taste-afric"
+    }
+]
