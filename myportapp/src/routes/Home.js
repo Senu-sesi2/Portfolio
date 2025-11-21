3f96935ad03f63369847cd551b632178b72e1fe3
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-21 13:13:46 +0000

    add project works to home page

--- myportapp/src/routes/Home.js
+++ myportapp/src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSec from "../components/HeroSec"
 import Footer from './../components/Footer';
+import Work from "../components/Work"
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <HeroSec />
+      <Work />
       <Footer />
     </div>
   )
