18c289528eadd5a39385f7dd4cfe53dab7abc25e
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-21 13:01:59 +0000

    add contact component and styles

--- myportapp/src/routes/Contact.js
+++ myportapp/src/routes/Contact.js
@@ -3,12 +3,14 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from './../components/Footer';
 import HeroSec2 from '../components/HeroSec2';
+import Form from '../components/Form';
 
 const Contact = () => {
   return (
     <div>
       <Navbar />
       <HeroSec2 heading="CONTACT" text="Drop me a message..."/>
+      <Form />
       <Footer />
     </div>
   )
