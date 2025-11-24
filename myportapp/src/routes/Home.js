48950053c86b16786cf868877531063a44c61f50
Senu-sesi2 <senu.sesi2@gmail.com>
2025-11-24 11:50:04 +0000

    add .env at the other end

--- myportapp/src/routes/Home.js
+++ myportapp/src/routes/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSec from "../components/HeroSec"
 import Footer from './../components/Footer';
-import Work from "../components/Work"
+// import Work from "../components/Work"
 
 const Home = () => {
   return (
